6d17a2b27be6158043aec8d6618080aaadb18aa1
NataPas2021 <natapas13@gmail.com>
2024-01-18 21:09:34 +0200

    ready

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,131 +1,13 @@
-import {useState} from 'react';
-//import { Component } from "react";
-import FeedbackOptions from "./FeedbackOptions/FeedbackOptions";
-import Statistics from "./Statistics/Statistics";
-import Section from "./Section/Section";
-import Notification from "./Notification/Notification";
-import PropTypes from 'prop-types';
-
-const feedbackOptions = ["good", "neutral", "bad"];
-
- const App = () => {
- const [options, setOptions] = useState({
-  good: 0,
-  neutral:0,
-  bad:0,
- })
-
- const {good, neutral, bad} = options;
- 
-
- const optionsFeedback = (option) => {
-  setOptions(prevOptions => ({
-    
-    ...prevOptions,
-    [option]: prevOptions[option] + 1
- }))
- }
-
- const countTotalFeedback = () => {
-  const total = good + neutral + bad;
-  console.log(total);
-  return total;
- }
-
- const countPositiveFeedbackPercentage = () => {
-  return Math.round(good*100/(good + neutral + bad));
- }
+import css from "./App.module.css";
+import Feedback from "./Feedback/Feedback";
 
 
-const existFeedback = countTotalFeedback();
-
+function App() {
  return (
-
-      <div className='app-container'>
-        <>
-          <Section title="Please, leave feedback">
-            <FeedbackOptions options={feedbackOptions} onLeaveFeedback={optionsFeedback}/>
-          </Section>
-        </>
-        {existFeedback === 0 ? (
-          <Notification message="Oops.There is no feedback yet"/>
-        ) : (
-          <>
-           <Section title="Statistics">
-            <Statistics good={good} neutral={neutral} bad={bad} total={countTotalFeedback} positivePercentage={countPositiveFeedbackPercentage}/>
-           </Section>
-          </>
-        )}
-        
-        
-        
-      </div>
-    )
+  <div className={css.appContainer}>
+    <Feedback />
+  </div>
+ )
 }
 
 export default App;
-
-//  export class App extends Component {
-//   state = {
-//     good: 0,
-//     neutral: 0,
-//     bad: 0
-//   }
-
-//   optionsFeedback = (e) => {
-//     const option = e.target.name;
-//     this.setState(prevState => {
-//       return {[option]: prevState[option] + 1};
-//         }
-//     )
-//   }
-       
-//   countTotalFeedback = () => { 
-//     return this.state.good + this.state.neutral + this.state.bad;
-//   };
-   
-//   countPositiveFeedbackPercentage = () => {
-//     return Math.round(this.state.good*100/(this.state.good + this.state.neutral + this.state.bad));
-//   };
-
-//   render() {
-//     const options = Object.keys(this.state);
-//     const existFeedback = this.countTotalFeedback();
-    
-//   return (
-
-//     <div className='app-container'>
-//       <>
-//         <Section title="Please, leave feedback">
-//           <FeedbackOptions options={options} onLeaveFeedback={this.optionsFeedback}/>
-//         </Section>
-//       </>
-//       {existFeedback === 0 ? (
-//         <Notification message="Oops.There is no feedback yet"/>
-//       ) : (
-//         <>
-//          <Section title="Statistics">
-//           <Statistics good={this.state.good} neutral={this.state.neutral} bad={this.state.bad} total={this.countTotalFeedback()} positivePercentage={this.countPositiveFeedbackPercentage()}/>
-//          </Section>
-//         </>
-//       )}
-      
-      
-      
-//     </div>
-//   )
-// }
-// }
-
-
-App.propTypes = {
-  title: PropTypes.string,
-  message: PropTypes.string,
-  options: PropTypes.object,
-  onLeaveFeedback: PropTypes.func,
-  good: PropTypes.number,
-  neutral: PropTypes.number,
-  bad: PropTypes.number,
-  total: PropTypes.func,
-  positivePercentage: PropTypes.func
-}
---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,81 @@
+import { useState } from 'react';
+//import css from './Feedback.module.css'
+import FeedbackOptions from "components/FeedbackOptions/FeedbackOptions";
+import Statistics from 'components/Statistics/Statistics';
+import Section from "components/Section/Section";
+import Notification from "components/Notification/Notification";
+import PropTypes from 'prop-types';
+
+const Feedback = () => {
+ const [options, setOptions] = useState({
+  good: 0,
+  neutral: 0,
+  bad: 0,
+ })
+
+ console.log(Object.keys(options)); 
+
+ const {good, neutral, bad} = options;
+
+ console.log(options);
+ 
+ const countTotalFeedback = () => {
+  const total = good + neutral + bad;
+  console.log(total);
+  return total;
+ }
+
+ const countPositiveFeedbackPercentage = (keyName) => {
+  const total = countTotalFeedback();
+  const value = options[keyName]; 
+  return Math.round(value*100/(total));
+ }
+
+ const leaveFeedback = (e) => {
+  const option = e.target.name;
+  setOptions(prevState => ({
+    ...prevState,
+    [option]: prevState[option] + 1
+ }))
+ 
+ }
+
+const feedBackOptions = Object.keys(options);
+const existFeedback = countTotalFeedback();
+const positivePercentage = countPositiveFeedbackPercentage('good');
+
+
+ return (
+    <div>
+        <>
+          <Section title="Please, leave feedback">
+            <FeedbackOptions options={feedBackOptions} onLeaveFeedback={leaveFeedback}/>
+          </Section>
+        </>
+        {existFeedback === 0 ? (
+          <Notification message="Oops.There is no feedback yet"/>
+        ) : (
+          <>
+           <Section title="Statistics">
+            <Statistics good={good} neutral={neutral} bad={bad} total={existFeedback} positivePercentage={positivePercentage}/>
+           </Section>
+          </>
+        )}
+    </div>    
+    )
+}
+
+export default Feedback;
+
+
+Feedback.propTypes = {
+  title: PropTypes.string,
+  message: PropTypes.string,
+  options: PropTypes.object,
+  onLeaveFeedback: PropTypes.func,
+  good: PropTypes.number,
+  neutral: PropTypes.number,
+  bad: PropTypes.number,
+  total: PropTypes.func,
+  positivePercentage: PropTypes.func
+}
